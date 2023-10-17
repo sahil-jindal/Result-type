b956b82cb2720d75df6c0226d4f7e2b9cfb9b988
Sahil Jindal <sahiljindal.thebest@gmail.com>
2023-10-18 00:35:22 +0530

    Written possible examples

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,13 +3,58 @@ package org.example;
 import java.util.function.Function;
 
 public class Main {
+
     public static void main(String[] args) {
-        Result<Person> something = Result.success(new Person("Rahul", "Agarwal"));
-        System.out.println(something);
+        // Example usage of Result class
+        Result<Integer> successResult = Result.success(42);
+        Result<Integer> failureResult = Result.failure("Operation failed");
 
-        Function<Person, String> getLength = a -> a.firstname + " " + a.lastname;
-        Function<Exception, String> getMessageLength = Throwable::getMessage;
+        // Example usage of map function in Result class
+        Result<String> mappedResult = successResult.map(value -> "Result value: " + value);
 
-        System.out.println(something.fold(getLength, getMessageLength));
+        // Example usage of fold function in Result class
+        String foldedSuccess = successResult.fold(
+                value -> "Success: " + value,
+                exception -> "Failure: " + exception.getMessage()
+        );
+
+        String foldedFailure = failureResult.fold(
+                value -> "Success: " + value,
+                exception -> "Failure: " + exception.getMessage()
+        );
+
+        System.out.println("Mapped result: " + mappedResult);
+        System.out.println("Folded success: " + foldedSuccess);
+        System.out.println("Folded failure: " + foldedFailure);
+
+        // Example usage of ResultUtil class
+        Function<Exception, Integer> onFailureFunction = exception -> {
+            System.err.println("An error occurred: " + exception.getMessage());
+            return -1;
+        };
+
+        int valueOrDefault = ResultUtil.getOrElse(successResult, onFailureFunction);
+        System.out.println("Value or default: " + valueOrDefault);
+
+        int valueOrDefault2 = ResultUtil.getOrDefault(failureResult, 100);
+        System.out.println("Value or default 2: " + valueOrDefault2);
+
+        // Example usage of remaining functions from ResultUtil
+        Function<Integer, String> transformFunction = value -> "Transformed value: " + value;
+
+        Result<String> mappedCatchingResult = ResultUtil.mapCatching(successResult, transformFunction);
+        System.out.println("Mapped catching result: " + mappedCatchingResult);
+
+        Result<Integer> recoveredResult = ResultUtil.recover(failureResult, exception -> -1);
+        System.out.println("Recovered result: " + recoveredResult);
+
+        Result<Integer> recoveredCatchingResult = ResultUtil.recoverCatching(failureResult, exception -> -2);
+        System.out.println("Recovered catching result: " + recoveredCatchingResult);
+
+        Result<Integer> runCatchingResult = ResultUtil.runCatching(() -> {
+            throw new Exception("Simulated exception");
+        });
+
+        System.out.println("Run catching result: " + runCatchingResult);
     }
 }
